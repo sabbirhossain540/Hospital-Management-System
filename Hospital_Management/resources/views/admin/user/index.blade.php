ac33c87dd42ce0d637c706efc8e2bd02f8869022
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-04-12 17:24:51 +0600

    userlist

---
+++ Hospital_Management/resources/views/admin/user/index.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.layouts')
+
+@section("content")
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <div class="d-flex flex-row">
+                <div class="col-md-10">
+                    <h6 class="m-0 font-weight-bold text-primary">User List</h6>
+                </div>
+                <div class="col-md-2" style="margin-left: 65px;">
+                    <a href="#" class="btn btn-primary btn-sm pl-10">Create User</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($userlist as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->role }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-primary btn-sm">Edit</a>
+                                    <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- Hospital_Management/routes/web.php
+++ Hospital_Management/routes/web.php
@@ -21,4 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/userList', 'UserController@index')->name('userList');
+
 
