c4381ae3268750b30c1f29d4acb70a30d2c87fb5
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-07-31 01:54:35 +0600

    initial

--- Hospital_Management/app/Http/Controllers/HomeController.php
+++ Hospital_Management/app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoice;
+use App\InvoiceDetails;
+use App\Services;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $invoiceList = InvoiceDetails::all();
+        $salesAmount = 0;
+        foreach($invoiceList as $sales){
+            $salesAmount = $salesAmount + $sales->total;
+        }
+
+        $totalService = Services::all()->count();
+        $totalInvoice = Invoice::all()->count();
+        $totalUser = User::all()->count();
+        $totalDoctor = User::where('role','doctor')->count();
+        $totalPatient = User::where('role', 'patient')->count();
+
+        //Invoice
+        $invoiceMaster = Invoice::with('invoiceDetails', 'getDoctor', 'getPatient')->take(5)->orderBy('created_at', 'DESC')->get();
+
+        return view('admin.index', compact('salesAmount', 'totalService', 'totalInvoice', 'totalUser', 'totalDoctor', 'totalPatient', 'invoiceMaster'));
     }
 }
