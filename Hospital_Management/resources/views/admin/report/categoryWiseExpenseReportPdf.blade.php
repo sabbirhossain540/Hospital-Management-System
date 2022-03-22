d081030e327d957835bb1df616cc6f6bcc6d99cb
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-03-22 15:10:32 +0600

    sdf

--- Hospital_Management/resources/views/admin/report/categoryWiseExpenseReportPdf.blade.php
+++ Hospital_Management/resources/views/admin/report/categoryWiseExpenseReportPdf.blade.php
@@ -8,6 +8,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Category Wise Expense Report</title>
+    <style>
+        .custom_font_size{
+            font-size: 11px;
+        }
+    </style>
 </head>
 <body>
 <table>
@@ -21,7 +26,7 @@
 <h6 class="text-center">Expence Category: {{ $expCategoryName->name }}</h6>
 <h6 class="text-center mb-4">Date Range: {{ $fromDate }} -- {{ $originalToDate }}</h6>
 
-<table class="table table-sm table-bordered">
+<table class="table table-sm table-bordered custom_font_size">
     <thead>
     <tr>
         <th class="text-center" scope="col">SN</th>
